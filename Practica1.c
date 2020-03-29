1247fcef6dfe9f56258c174defebaf08ffff1405
adchavesm <adchavesm@unal.edu.co>
2020-03-29 03:25:58 -0500

    version 1.1

--- Practica1.c
+++ Practica1.c
@@ -3,48 +3,46 @@
 
 int ancho, alto;
 
-void leerImagen( char *nombreArchivo);
+void leerImagen(char *nombreArchivo);
 
-void imprimirImagen(char R[ancho][alto],char G[ancho][alto],char B[ancho][alto]);
+void imprimirImagen(char R[ancho][alto], char G[ancho][alto], char B[ancho][alto]);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     leerImagen(argv[1]);
     return 0;
 }
 
-void leerImagen( char *nombreArchivo){
+void leerImagen(char *nombreArchivo)
+{
     FILE *archivo;
-    archivo = fopen(nombreArchivo,"r");
+    archivo = fopen(nombreArchivo, "r");
     char letra;
     int tipo, maxcolor;
-    fscanf(archivo,"%c%d %d %d %d",&letra,&tipo,&ancho,&alto,&maxcolor);
-    
-    char R[ancho][alto],G[ancho][alto],B[ancho][alto];
-    
-    
+    fscanf(archivo, "%c%d %d %d %d", &letra, &tipo, &ancho, &alto, &maxcolor);
+    char R[ancho][alto], G[ancho][alto], B[ancho][alto];
     for (int i = 0; i < ancho; i++)
     {
-         for (int j = 0; j < alto; j++)
-         {
-             fscanf(archivo,"%c%c%c", &R[i][j],&G[i][j],&B[i][j]);
-         }
-         
+        for (int j = 0; j < alto; j++)
+        {
+            fscanf(archivo, "%c%c%c", &R[i][j], &G[i][j], &B[i][j]);
+        }
     }
     fclose(archivo);
-    imprimirImagen(R,G,B);
+    imprimirImagen(R, G, B);
 }
 
-void imprimirImagen(char R[ancho][alto],char G[ancho][alto],char B[ancho][alto]){
+void imprimirImagen(char R[ancho][alto], char G[ancho][alto], char B[ancho][alto])
+{
     FILE *ap;
-    ap=fopen("salida.ppm","w");
-    fprintf(ap,"P6\n%d %d\n255",ancho,alto);
+    ap = fopen("salida.ppm", "w");
+    fprintf(ap, "P6\n%d %d\n255", ancho, alto);
     for (int i = 0; i < ancho; i++)
     {
-         for (int j = 0; j < alto; j++)
-         {
-             fprintf(ap,"%c%c%c", R[i][j],G[i][j],B[i][j]);
-         }
-     //    fprintf(ap,"\n");
+        for (int j = 0; j < alto; j++)
+        {
+            fprintf(ap, "%c%c%c", R[i][j], G[i][j], B[i][j]);
+        }
     }
     fclose(ap);
 }
